27d3e43e7b58fdf56e4d2661a5bf6adda29d5322
tejaswi gutta <tejaswigutta788@gmail.com>
2021-08-12 03:04:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import react from "react"
+import Pronote from "./Pronote.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [text,setText]=react.useState("")
+ function addText(event){
+   setText(event.target.value)
+ }
+ function reset(){
+  setText("")
+ }
+ return<div className="whole">
+   <div className="input-div">
+   <div>INPUT</div>
+   <textarea className="input" value={text} type="text" onChange={addText}/>
+   <img onClick={reset}src="https://previews.123rf.com/images/piterk/piterk1608/piterk160800249/61898293-refresh-or-reload-symbol-sign-one-line-icon-on-background.jpg" />
+   </div>
+   <div className="pronote-div">
+   <div>PRONOTE</div>
+   <Pronote text={text} />
+   </div>
+ </div>
 }
 
 export default App;
---
+++ src/Pronote.js
@@ -0,0 +1,5 @@
+import react from "react"
+function Pronote({text}){
+  return <div className="proNote">{text}</div>
+}
+export default Pronote
